7af5e22643754c7aac10978762d7e2d9475663a2
tkj <knappjt@yahoo.com>
2010-01-11 13:10:50 -0600

    added Friday date warnings
    Signed-off-by: tkj <knappjt@yahoo.com>

--- separate Scripts/terms&editScript.js
+++ separate Scripts/terms&editScript.js
@@ -1,5 +1,25 @@
 javascript:
-
+var attributesForInsertion = {};
+attributesForInsertion['division'] = "4";
+attributesForInsertion['sfOpportunityId'] = "0068000000RYyGq";
+attributesForInsertion['feature'] = "Feature";
+attributesForInsertion['featureDate'] = "2/1/2010";
+attributesForInsertion['featureEndDate'] = " ";
+attributesForInsertion['vendorName'] = "National Museum of Crime and Punishment";
+attributesForInsertion['redemptionAddress'] = "575 7th St NW \nWashington DC 20004";
+attributesForInsertion['billingAddress'] = "575 7th St NW \nWashington DC 20004";
+attributesForInsertion['unitSellPrice'] = "10";
+attributesForInsertion['unitValue'] = "20";
+attributesForInsertion['unitBuyPrice'] = "6.00";
+attributesForInsertion['dealMin'] = "50";
+attributesForInsertion['dealMax'] = " ";
+attributesForInsertion['buyerMax'] = " ";
+attributesForInsertion['vendorURL'] = "www.crimemuseum.org";
+attributesForInsertion['emailListTo'] = "janine@crimemuseum.org";
+attributesForInsertion['dealTitle'] = "National Museum of Crime and Punishment-$10 voucher for Admission RUN IN FEB";
+attributesForInsertion['conditions'] = "* Expires 05/21/2010 \n * Not valid with any other offers \n * Must exchange for ticket at museum \nExcludes holiday weekends \n-Check website for hours of operation";
+attributesForInsertion['instructions'] = "1. Print Groupon \n2. Check Museum hours http://www.crimemuseum.org/purchase_online.html \n3. Advance tickets recommended-must go to museum to trade in Groupon for Tickets \n3. Enjoy";
+attributesForInsertion['rawExpirationDate'] = "May 21 2010";
 /* Insert ATTRIBUTESFORINSERTION here */
 
 /* Terms/Edit Page */
@@ -7,6 +27,9 @@ $('campaign_division_id').value=attributesForInsertion['division'];
 $('campaign_opportunity_id').value=attributesForInsertion['sfOpportunityId'];
 if (attributesForInsertion["feature"]=="Feature") $('campaign_top_deal').checked="TRUE";
 var launchDate = new Date(attributesForInsertion['featureDate']);
+if (launchDate.getDay()==5) {
+	$('campaign_deadline_date_3i').style.backgroundColor='pink'; 
+	$$('div.launch_date')[0].childNodes[1].innerHTML+=' <span style="color:red">Friday Deal!</span>'; }
 var endDate = new Date(attributesForInsertion['featureEndDate']);
 $('campaign_schedule_date_1i').value=launchDate.getFullYear();
 $('campaign_schedule_date_2i').value=launchDate.getMonth()+1;
