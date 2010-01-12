5ce864acd14c6d7e8c68c986b11c73e43797e3d5
tkj <knappjt@yahoo.com>
2010-01-12 12:30:52 -0600

    Created script to populate The Point, added the point fields to resources
    Signed-off-by: tkj <knappjt@yahoo.com>

---
+++ separate Scripts/thePoint.js
@@ -0,0 +1,15 @@
+javascript:
+
+/* Insert ATTRIBUTESFORINSERTION here */
+
+if ($('vendor_contact_params_new_permalink').value != "") {
+	$('vendor_name').value=attributesForInsertion["vendorName"];
+	$('vendor_address').value=attributesForInsertion["redemptionAddress"]; }
+else {
+	$('contact_full_name').value=attributesForInsertion["vendorName"];
+	$('contact_full_name').style.backgroundColor="pink";
+	$('contact_email_address').value=attributesForInsertion["emailListTo"];
+	var pass1 = attributesForInsertion["vendorName"].replace(/\s*/g,'') + "rocks" + Math.round(Math.random()*10);
+	var pass2 = pass1.toLowerCase();
+	$('contact_password').value=pass2;
+	$('contact_password_confirmation').value=pass2;}; return null;
